4fe240ced06f4368b442cfab272c635e6d2e0234
Sho0226 <s1F102201616@iniad.org>
2024-10-15 05:50:03 +0900

    add: Hello

---
+++ react-sample/src/components/Hello.tsx
@@ -0,0 +1,13 @@
+const Hello = ()=>{
+    const onclick = () =>{
+        alert('Hello')
+    }
+    const text = 'Hello. React'
+
+    return(
+        <div onClick={onclick}>
+            {text}
+        </div>
+    )
+}
+export default Hello
