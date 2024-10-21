492975f2627df25b80e3bfeaa4b473a92d6400b0
sho0226 <s1f102201616@iniad.org>
2024-10-22 00:16:50 +0900

    add: Clock

--- react-sample/src/index.tsx
+++ react-sample/src/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Parent from './components/Parent';
+import { Clock } from './components/Clock';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Parent />
+    <Clock />
   </React.StrictMode>
 );
 
